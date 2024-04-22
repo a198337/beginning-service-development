21afc6091bd3342700f870a7738b87bd1b9a9839
Helen K <helen_kim@progressive.com>
2024-04-22 16:18:18 -0400

    set of docker db and more posts

--- src/help-desk/IssueTrackerSolution/IssueTrackerApi/Controllers/Issues/Api.cs
+++ src/help-desk/IssueTrackerSolution/IssueTrackerApi/Controllers/Issues/Api.cs
@@ -1,14 +1,114 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FluentValidation;
+using Marten;
+using Microsoft.AspNetCore.Mvc;
 
 namespace IssueTrackerApi.Controllers.Issues;
 
-public class Api : ControllerBase
+public class Api(IDocumentSession session) : ControllerBase
 {
-    //GET /issues
+
+    // GET /issues
     [HttpGet("/issues")]
-    public async Task<ActionResult> GetTheIssuesAsync()
+    public async Task<ActionResult> GetTheIssuesAsync([FromQuery] string software = "all")
     {
-        var issues = new List<object>();
-        return Ok(issues);
+        if (software == "all")
+        {
+            var issues = await session.Query<Issue>().ToListAsync();
+            return Ok(issues);
+        }
+        else
+        {
+            var issues = await session.Query<Issue>().Where(i => i.Software == software).ToListAsync();
+            return Ok(issues);
+        }
+    }
+
+    [HttpPost("/issues/{id:guid}")]
+    public async Task<ActionResult> GetIssueByIdAsync(Guid id)
+    {
+        var issue = await session.Query<Issue>().SingleOrDefaultAsync(i => i.Id == id);
+
+        if (issue is null)
+        {
+            return NotFound();
+        }
+        else
+        {
+            return Ok(issue);
+        }
+    }
+
+    [HttpPost("/issues")]
+    public async Task<ActionResult> AddIssueAsync(
+       [FromBody] CreateIssueRequestModel request,
+       [FromServices] IValidator<CreateIssueRequestModel> validator)
+    {
+        var results = await validator.ValidateAsync(request);
+        if (results.IsValid)
+        {
+            var response = new Issue
+            {
+                CreatedAt = DateTimeOffset.UtcNow,
+                Description = request.Description,
+                Software = request.Software,
+                Id = Guid.NewGuid(),
+                Status = IssueStatus.Created
+            };
+            // do our thing.
+            session.Store(response);
+            await session.SaveChangesAsync();
+            return Ok(response);
+        }
+        else
+        {
+            return BadRequest(results.ToDictionary()); // 400
+        }
+
+    }
+}
+
+/*
+ * {
+  "software": "Excel",
+  "description": "I want clippy back"
+} */
+
+public record CreateIssueRequestModel
+{
+
+    public string Software { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
+
+
+public record Issue
+{
+    public Guid Id { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public string Software { get; set; } = string.Empty;
+    public DateTimeOffset CreatedAt { get; set; }
+    public IssueStatus Status { get; set; }
+
+}
+public enum IssueStatus { Created }
+
+public class CreateIssueRequestModelValidator : AbstractValidator<CreateIssueRequestModel>
+{
+    private readonly IReadOnlyList<string> _supportedSoftware = ["excel", "powerpoint", "word"];
+    public CreateIssueRequestModelValidator()
+    {
+        RuleFor(i => i.Description)
+            .NotEmpty()
+            .MaximumLength(1024);
+
+        RuleFor(i => i.Software)
+            .NotEmpty()
+            .Must(i =>
+            {
+                var sw = i.ToLowerInvariant().Trim();
+                return _supportedSoftware.Any(s => s == sw);
+
+            }).WithMessage("Unsupported Software. Good Luck");
+
     }
 }
