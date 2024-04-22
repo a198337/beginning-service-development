2cfb676ff2fbf2173eef16eddab993f709d0ec48
Helen K <helen_kim@progressive.com>
2024-04-22 14:04:57 -0400

    New code from Jeff for starting the project in class

---
+++ src/help-desk/IssueTrackerSolution/IssueTrackerApi/Controllers/Issues/Api.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IssueTrackerApi.Controllers.Issues;
+
+public class Api : ControllerBase
+{
+    //GET /issues
+    [HttpGet("/issues")]
+    public async Task<ActionResult> GetTheIssuesAsync()
+    {
+        var issues = new List<object>();
+        return Ok(issues);
+    }
+}
