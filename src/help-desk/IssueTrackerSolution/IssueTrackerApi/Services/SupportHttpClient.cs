9c2d0aa823613e4edef1231088111e3f89bf40a1
Helen K <helen_kim@progressive.com>
2024-04-23 17:01:05 -0400

    day 2 finishes, fixed some errors from missing references

---
+++ src/help-desk/IssueTrackerSolution/IssueTrackerApi/Services/SupportHttpClient.cs
@@ -0,0 +1,26 @@
+﻿namespace IssueTrackerApi.Services;
+
+public class SupportHttpClient
+{
+    private readonly HttpClient _httpClient;
+
+    public SupportHttpClient(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<CurrentSupportResponse?> GetCurrentSupportInformationAsync()
+    {
+        var response = await _httpClient.GetAsync("/");
+        response.EnsureSuccessStatusCode();
+        var content = await response.Content.ReadFromJsonAsync<CurrentSupportResponse>();
+        return content;
+    }
+}
+
+public record CurrentSupportResponse
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+}
